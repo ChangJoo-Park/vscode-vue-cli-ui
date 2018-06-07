a3bbee66181af7fcef99ad38e762cfddce38ae6d
ChangJoo Park <pcjpcj2@gmail.com>
2018-06-07 23:17:50 +0900

    Update extension command

--- extension.js
+++ extension.js
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const {
+    exec
+} = require('child_process')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,11 +16,12 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    let disposable = vscode.commands.registerCommand('extension.openVueUI', function () {
+        exec('vue ui', (error, stdout, stderr) => {
+            console.log(error)
+            console.log(stdout)
+            console.log(stderr)
+        })
     });
 
     context.subscriptions.push(disposable);
@@ -25,6 +29,5 @@ function activate(context) {
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {
-}
+function deactivate() {}
 exports.deactivate = deactivate;
